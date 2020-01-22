0305e64de17220b694e5c9ef5321ad861fab0a7c
Kerrie Low <klow4@mail.greenriver.edu>
2020-01-22 12:59:41 -0500

    Fat-free setup

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Kerrie Low
+ * 1.22.20
+ * Full Stack Software Development
+ * http://www.klow.greenriverdev.com/328/food/
+ */
+
+// turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// require the autoload file
+require_once('vendor/autoload.php');
+
+// instantiate F3
+// :: means static method
+$f3 = Base::instance();
+
+// define a default route
+// -> calls an instance method
+$f3->route('GET /', function() {
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+// run f3
+$f3->run();
