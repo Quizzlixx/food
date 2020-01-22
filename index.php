6d9643f655cfc7f8e6378829b2537296268ba533
Kerrie Low <klow4@mail.greenriver.edu>
2020-01-22 14:07:38 -0500

    order view added

--- index.php
+++ index.php
@@ -30,5 +30,48 @@ $f3->route('GET /breakfast', function() {
     echo $view->render('views/breakfast.html');
 });
 
+// define a breakfast buffet route
+$f3->route('GET /breakfast/buffet', function() {
+    $view = new Template();
+    echo $view->render('views/breakfast-buffet.html');
+});
+
+// define a route that accepts a food parameter
+$f3->route('GET /@item', function($f3, $params) {
+//    var_dump($params);
+    $item = $params['item'];
+    echo "<p>You ordered $item</p>";
+
+    $foodsWeServe = array("tacos", "pizza", "lumpia", "bagels");
+    if (!in_array($item, $foodsWeServe)) {
+        echo "<p>Sorry... we don't serve $item</p>";
+    }
+
+    switch($item) {
+        case 'tacos':
+            echo "<p>We serve tacos on Tuesdays</p>";
+            break;
+        case 'pizza':
+            echo "<p>Pepperoni or Veggie?";
+            break;
+        case 'bagels':
+            $f3->reroute("/breakfast");
+        default:
+            $f3->error(404);
+    }
+});
+
+// define a lunch route
+$f3->route('GET /lunch', function() {
+    $view = new Template();
+    echo $view->render('views/lunch.html');
+});
+
+// define a route called order displays a view called form1.html
+$f3->route('GET /order', function() {
+    $view = new Template();
+    echo $view->render('views/form1.html');
+});
+
 // run f3
 $f3->run();
